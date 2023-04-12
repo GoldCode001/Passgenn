8c3d3deb92e5e5819b0641bd0c758b753c7fd4bb
GoldCode001 <129571583+GoldCode001@users.noreply.github.com>
2023-04-12 19:15:24 +0100

    Update passgen.py

--- Tools/Passgen/passgen.py
+++ Tools/Passgen/passgen.py
@@ -23,7 +23,7 @@ def local_css(file_name):
     with open(file_name)as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("Pass/style.css")
+local_css("Tools/Pass/style.css")
 # Create a Streamlit app
 def main():
     st.set_page_config(page_title="Password Generator", page_icon="🔒")
